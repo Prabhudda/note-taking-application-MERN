66a4c1e30b3f528da65fe7ce7c7d468a7c0f10d1
PRABHUDDA <79395762+PRABHUDDA@users.noreply.github.com>
2024-04-12 23:26:09 +0530

    new update

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NoteProvider from './Context';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
     <NoteProvider>
+      <ToastContainer />
       <App />
     </NoteProvider>
   </BrowserRouter>
